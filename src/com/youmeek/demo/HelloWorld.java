68be5ce78a094f0413490f332082b11b35abd3ea
柴克俭 <chaikejian@suiyueyule.com>
2017-11-03 16:57:38 +0800

    312

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -2,6 +2,15 @@ package com.youmeek.demo;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello Java 柴克俭");
+        System.out.print("Hello Java 123");
+
+        System.out.print("Hello Java 234");
+
+        int temp1 = 100;
+        System.out.println("-----------cya-----------temp1值=" + temp1 + "," + "当前类=HelloWorld.main()");
+
+        int temp2 = 100;
+        System.out.println("-----------cya-----------temp2值=" + temp2 + "," + "当前类=HelloWorld.main()");
+
     }
 }
